6385ee0f0df31416f06b94324e486cdafc5994a0
student <sfdsdf@sdfsd.ru>
2017-04-29 22:17:07 +0300

    Initial commit

---
+++ header.hh
@@ -0,0 +1,11 @@
+//
+// Created by student on 29.04.17.
+//
+
+#ifndef UNTITLED12_HEADER_HH
+#define UNTITLED12_HEADER_HH
+void tabl1();
+void tabl2();
+void sw(int a);
+
+#endif //UNTITLED12_HEADER_HH
---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "header.hh"
+
+int main() {
+
+    tabl1();
+    tabl2();
+    printf("\nВведите число от 1 до 2: ");
+    int a;
+    scanf("%d", &a);
+    sw(a);
+
+    return 0;
+}
